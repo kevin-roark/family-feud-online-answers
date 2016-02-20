7e18018bff2a4388f73bfb430799325fef073570
Kevin Roark <ker2143@columbia.edu>
2016-02-20 12:51:12 -0600

    framptonify and copy relevant from family.feud.online

---
+++ score.js
@@ -0,0 +1,30 @@
+
+var renderer = new frampton.WebRenderer({
+  mediaConfig: mediaConfig,
+  timeToLoadVideo: 3600
+});
+
+var firstSegment = newSequencedSegment();
+renderer.scheduleSegmentRender(firstSegment, 2000);
+
+function newSequencedSegment() {
+  var segments = [];
+
+  var videos = frampton.util.shuffle(mediaConfig.videos);
+  videos.forEach((video) => {
+    var segment = new frampton.VideoSegment(video);
+    segments.push(segment);
+  });
+
+  var sequencedSegment = new frampton.SequencedSegment({
+    segments: segments,
+    onStart: () => {
+      // once it starts, schedule the next loop with a new shuffle
+      var newSegment = newSequencedSegment();
+      var offset = sequencedSegment.msDuration();
+      renderer.scheduleSegmentRender(newSegment, offset);
+    }
+  });
+
+  return sequencedSegment;
+}
